609c4aeff2104b7169b8a6f333976e97f8ac6466
Shari Afuso <shariafuso@Sharis-MacBook-Pro.local>
2016-05-27 16:39:23 -0700

    moved jquery into new file

---
+++ js/show-and-hide.js
@@ -0,0 +1,10 @@
+<script>
+	$(document).ready(function(){
+	    $("#hide-me button").click(function(){
+	        $("h1").hide(1000);
+	    });
+	    $("#show-me button").click(function(){
+	        $("h1").show(1000);
+	    });
+	});
+</script>
